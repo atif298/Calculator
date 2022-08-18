757ac74cf4e50d44aad6ce106b78861c56daa1e9
atif298 <atifuddin03@gmail.com>
2022-08-18 15:54:18 +0100

    DOM all elements for calculator

---
+++ script.js
@@ -0,0 +1,18 @@
+//DOM all calculator components
+
+//Numbers button
+const numbers = document.getElementsByClassName("calculator__number")
+//Operators button
+const operators = document.getElementsByClassName("calculator__operator")
+//Clear button
+const clear = document.getElementById("C")
+//Delete button
+const deleteNumber = document.getElementById("DEL")
+//Equals button
+const equals = document.getElementById("equals")
+//Previous output
+const previousScreen = document.getElementsByClassName("calculator__output-current")
+//Current output
+const currentScreen = document.getElementsByClassName("calculator__output-previous")
+
+
